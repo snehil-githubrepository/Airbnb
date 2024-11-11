d80f571d5a54c075144e1aa31a666797e7ceddcb
Snehil <snehil.sharma020901@gmail.com>
2024-11-11 05:49:35 +0530

    Register Page update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    console.log("registering user...");
+  };
+
   return (
     <div className="mt-4 grow flex items-center justify-center">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-        <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button className="primary mt-2">Login</button>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="primary mt-2">Register</button>
           <div className="text-center py-2 text-gray-500">
-            Already a Member? <Link className="underline text-black" to={'/login'}>Login</Link>
+            Already a Member?{" "}
+            <Link className="underline text-black" to={"/login"}>
+              Login
+            </Link>
           </div>
         </form>
       </div>
